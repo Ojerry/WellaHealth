1c10b9800cbf878ff19d850e34558a998c392b34
Ojerry <onotu128@gmail.com>
2022-04-29 13:25:37 -0700

    Added Lab 3 Prime Numbers

---
+++ Lab3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool isPrime = true;
+            Console.WriteLine("Enter the Number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if(number < 1) 
+            Console.WriteLine("Number needs to be greater than 1");
+            else if (number == 1)
+            Console.WriteLine("1 is neither Prime nor Composite");
+            else{
+                for( int divisor =2; divisor <= (number / 2); divisor++) {
+                    if ( number % divisor == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                    Console.WriteLine("{0} is a prime number", number);
+                else
+                    Console.WriteLine("{0} is a Composite number", number);
+            }
+        }
+    }
+}
