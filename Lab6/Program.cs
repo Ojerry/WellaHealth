a7a9328108cf1f8dcd1d2118f2b31fb52ed2e865
Ojerry <onotu128@gmail.com>
2022-07-06 13:05:52 +0100

    Abstraction

---
+++ Lab6/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Lab6
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            shape [] arrShapes = {new rectangle(), new triangle(), new circle(2,4,8)};
+            int [] theAreas = {arrShapes[0].CalculateSurface(6,5), arrShapes[1].CalculateSurface(6,5), arrShapes[2].CalculateSurface(3,6)};
+                int i = 0;
+            foreach(int area in theAreas){               
+                Console.WriteLine("The area of shape in index " + i + " is " + area);
+                i++;
+            }
+        }
+    }
+
+         abstract class shape{
+        public abstract int CalculateSurface(int width,int height);
+    }
+
+    class rectangle : shape{
+        override public int CalculateSurface(int width, int height){
+            return width * height;
+        }
+    }
+
+    class triangle : shape{
+        override public int CalculateSurface(int width, int height){
+            return (width * height) / 2;
+        }
+    }
+
+        abstract class circleArea{
+                public abstract int CalcArea();
+            }
+        class circle : shape
+        {
+        private int width = 0;
+        private int height = 0;
+        private int radius = 0;
+
+        public circle(int wth, int hht, int rad)
+        {
+            width = wth;
+            height = hht;
+            radius = rad;
+        }
+
+        override public int CalculateSurface(int width, int height)
+        {
+            return Convert.ToInt32(3.14 * (radius * radius));
+        }
+    }
+}
